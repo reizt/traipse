87ea69c47c9b66e70f8ee7aa4bfd6ed43c4dabc3
reizt <reizt.dev@gmail.com>
2023-09-22 16:40:05 +0900

    Export fetcher error

--- index.ts
+++ index.ts
@@ -4,7 +4,7 @@ export { LogicError, LogicErrorBuilder, type LogicErrorCodeDef, type LogicErrorK
 
 export { TraipseClient } from './src/client/client';
 export { fetchFetcher } from './src/client/fetcher/fetch';
-export type { Fetcher } from './src/client/types';
+export { FetcherError, type Fetcher } from './src/client/types';
 
 export { TraipseServer } from './src/server/server';
 export type { Logic } from './src/server/types';
